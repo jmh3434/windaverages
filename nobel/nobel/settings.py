479ff8fe6fdb59164515fa960d326c17ed94cf39
jmh3434 <jmh3434@gmail.com>
2018-05-03 22:27:38 -0400

    first commit

--- nobel/nobel/settings.py
+++ nobel/nobel/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%z3o1v^kz#ma&%@wqil&2+2m4=h6gg4$=qrh#bd*du!!pq%8oh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
